56e0555756b2ac92f748659acbef861d57b6f276
Chriztian Steinmeier <chriztian@steinmeier.dk>
2022-02-14 00:07:56 +0100

    Hide letters by default and provide click-to-view JS

--- wordles.Frontend/src/js/modules/app.js
+++ wordles.Frontend/src/js/modules/app.js
@@ -1,13 +1,17 @@
 //
 //	This is the app's main JS file.
-//	
+//
 //	It must export a single `default function` that gets imported and
 //	executed by the two wrapper files in the parent `js` folder.
 //
 
-import Point from './point.js'
-
 export default function() {
-	const pointTest = new Point(5, 10)
-	console.log(pointTest.draw())
+	document.body.addEventListener('click', (event) => {
+		const target = event.target
+		const gamePanel = target.closest('.game-panel')
+
+		if (gamePanel != null) {
+			gamePanel.classList.toggle('hide')
+		}
+	})
 }
